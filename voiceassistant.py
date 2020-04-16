481c6439670413db7b291427d00e8f23717f194a
Deepika-579 <60223071+Deepika-579@users.noreply.github.com>
2020-04-16 10:26:42 +0530

    Add files via upload

---
+++ voiceassistant.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr 
+import webbrowser
+sr.Microphone(device_index=1)
+r=sr.Recognizer()
+r.energy_threshold=5000
+with sr.Microphone() as source:
+    print("say something")
+    audio=r.listen(source)
+    try:
+        text=r.recognize_google(audio)
+        print("you said:{}".format(text))
+        url='https://www.google.com/search?q='
+        search_url=url+text
+        webbrowser.open(search_url)
+    except:
+        print("say loud and clearly")
